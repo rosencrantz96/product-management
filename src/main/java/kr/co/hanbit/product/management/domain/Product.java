0f9ca888f9c6fa5d11b8f4bb79ddb642c494b913
권수경 <topgdvidsyb@naver.com>
2024-06-07 17:18:48 +0900

    상품관리 애플리케이션 - 데이터베이스 연동하기, 상품 추가 기능

--- src/main/java/kr/co/hanbit/product/management/domain/Product.java
+++ src/main/java/kr/co/hanbit/product/management/domain/Product.java
@@ -20,6 +20,18 @@ public class Product {
     @Min(0)
     private Integer amount;
 
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
