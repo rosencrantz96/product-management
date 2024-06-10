b63196985bdca63e679d30c66ac5192b78ec010b
권수경 <topgdvidsyb@naver.com>
2024-06-10 10:27:26 +0900

    상품관리 애플리케이션 - 상품 조회 기능

--- src/main/java/kr/co/hanbit/product/management/infrastructure/DatabaseProductRepository.java
+++ src/main/java/kr/co/hanbit/product/management/infrastructure/DatabaseProductRepository.java
@@ -2,8 +2,10 @@ package kr.co.hanbit.product.management.infrastructure;
 
 import kr.co.hanbit.product.management.domain.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -38,15 +40,35 @@ public class DatabaseProductRepository {
     }
 
     public Product findById(Long id) {
-        return null;
+        SqlParameterSource namedParameter = new MapSqlParameterSource("id", id);
+
+        Product product = namedParameterJdbcTemplate.queryForObject(
+                "SELECT id, name, price, amount FROM products WHERE id=:id",
+                namedParameter,
+                new BeanPropertyRowMapper<>(Product.class)
+        );
+
+        return product;
     }
 
     public List<Product> findAll() {
-        return Collections.EMPTY_LIST;
+        List<Product> products = namedParameterJdbcTemplate.query(
+                "SELECT * FROM products",
+                new BeanPropertyRowMapper<>(Product.class)
+        );
+
+        return products;
     }
 
     public List<Product> findByNameContaining(String name) {
-        return Collections.EMPTY_LIST;
+        SqlParameterSource namedParameter = new MapSqlParameterSource("name", "%" + name + "%");
+
+        List<Product> products = namedParameterJdbcTemplate.query(
+                "SELECT * FROM products WHERE name LIKE :name",
+                namedParameter,
+                new BeanPropertyRowMapper<>(Product.class)
+        );
+        return products;
     }
 
     public Product update(Product product) {
