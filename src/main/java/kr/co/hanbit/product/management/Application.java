5bdbf730a395aaaf136ac5adbe2d69216b1cecee
권수경 <topgdvidsyb@naver.com>
2024-06-17 17:13:51 +0900

    객체지향 - ModelMapper 제거하기

--- src/main/java/kr/co/hanbit/product/management/Application.java
+++ src/main/java/kr/co/hanbit/product/management/Application.java
@@ -1,7 +1,5 @@
 package kr.co.hanbit.product.management;
 
-import org.modelmapper.ModelMapper;
-import org.modelmapper.config.Configuration;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,15 +16,15 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	public ModelMapper modelMapper() {
-		ModelMapper modelMapper = new ModelMapper();
-		modelMapper.getConfiguration()
-				.setFieldAccessLevel(Configuration.AccessLevel.PRIVATE)
-				.setFieldMatchingEnabled(true);
-
-		return modelMapper;
-	}
+//	@Bean
+//	public ModelMapper modelMapper() {
+//		ModelMapper modelMapper = new ModelMapper();
+//		modelMapper.getConfiguration()
+//				.setFieldAccessLevel(Configuration.AccessLevel.PRIVATE)
+//				.setFieldMatchingEnabled(true);
+//
+//		return modelMapper;
+//	}
 
 	@Bean
 	@Profile("prod")
